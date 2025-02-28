01231e8670ec52ab6554b41d8c3a5266e6c1afa3
Harish Saud <saudharish770@gmail.com>
2025-02-28 22:50:22 +0545

    Update ButtonExample.java

--- ButtonExample.java
+++ ButtonExample.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 public class ButtonExample {
 
     public static void main(String[] args) {
-        // Create a new frame
+
         JFrame frame = new JFrame("Button Example");
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
